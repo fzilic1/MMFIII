b26109fb3568bf3637d456157ebdae5591bc36f3
fzilic1 <100929385+fzilic1@users.noreply.github.com>
2022-10-12 11:57:10 +0200

    Update vj3.py

--- vj1/vj3.py
+++ vj1/vj3.py
@@ -19,3 +19,28 @@ for i in range(len(a)):
     file.write("%8.11f\t%8.11f\n" %(a[i], b[i]))
 
 file.close()
+
+def lagrange(ax, by, N, x, y):
+    for i in range(N):
+        L=1
+        for j in range(N):
+            if j != i:
+                L=L*(x-ax[j])/(ax[i]-ax[j])
+
+        y+=by[i]*L
+    return y
+
+
+k=7/71
+z=2.81
+file = open("V(H-H)_inter.txt", "w")
+while z <= 9.81:
+    Y=0
+    Y=lagrange(a, b, len(a), z, Y)
+    
+    file.write("%8.11f\t%8.11f\n" %(z, Y))
+    
+    
+    z+=k
+
+file.close()

080dcc530d5c56c2bb0d7600da086b80c4a26410
fzilic1 <100929385+fzilic1@users.noreply.github.com>
2022-11-09 11:35:00 +0100

    Create vj5.py

---
+++ vj1/vj5.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def y1(t):
+    y=5.0+np.cos(3.0*t)-0.325-2.0*np.exp(0.5*t)
+    return y
+
+def dy1(t):
+    y=-3.0*np.sin(3.0*t)-np.exp(0.5*t)
+    return y
+
+def bisekcija(func, epsilon, a, b):
+    a=a
+    b=b
+    c=(a+b)/2
+    temp1=[]
+    temp2=[]
+    n=0
+    while abs(func(c)) >= epsilon:
+        if func(a)*func(c) < 0:
+            b=c
+        else:
+            a=c
+        c=(a+b)/2
+        n+=1
+
+    return [c, n]
+
+print (bisekcija(y1, 0.0001, 0, 1000))
+
+def Newton(func, der, epsilon, x0):
+    x=x0
+    n=0
+    while abs(func(x)/der(x)) >= epsilon:
+        x=x-(func(x)/der(x))
+        n+=1
+
+    return [x, n]
+
+print (Newton(y1, dy1, 0.0001, 8))   
